c6a2c35c9e3771950df86830df171c6b28e8a9ff
krjarnold <krjarnold@gmail.com>
2017-02-21 14:46:26 -0500

    Initial commit

---
+++ frontend/components/app.jsx
@@ -0,0 +1,12 @@
+const React = require('react');
+
+const App = React.createClass({
+
+  render() {
+    return (
+      <div className="app-container">This is my heart beat app</div>
+    );
+  }   
+});
+
+module.exports = App;
