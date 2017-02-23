c812a87863810e36bf4e9c5f2639f06ed5b7172e
krjarnold <krjarnold@gmail.com>
2017-02-23 14:43:10 -0500

    Add csv

--- frontend/HeartBeat.jsx
+++ frontend/HeartBeat.jsx
@@ -1,26 +1,26 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-
-//Router
-const ReactRouter = require('react-router');
-const Router = ReactRouter.Router;
-const Route = ReactRouter.Route;
-const IndexRoute = ReactRouter.IndexRoute;
-const hashHistory = ReactRouter.hashHistory;
-
-//Components
-const App = require('./components/app');
-
-
-const appRouter = (
-  <Router history={hashHistory}>
-    <Route path="/" component={ App }>
-    </Route>
-  </Router>
-);
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  const root = document.getElementById("root");
-  ReactDOM.render(appRouter, root);
-});
+// const React = require('react');
+// const ReactDOM = require('react-dom');
+//
+// //Router
+// const ReactRouter = require('react-router');
+// const Router = ReactRouter.Router;
+// const Route = ReactRouter.Route;
+// const IndexRoute = ReactRouter.IndexRoute;
+// const hashHistory = ReactRouter.hashHistory;
+//
+// //Components
+// const App = require('./components/app');
+//
+//
+// const appRouter = (
+//   <Router history={hashHistory}>
+//     <Route path="/" component={ App }>
+//     </Route>
+//   </Router>
+// );
+//
+//
+// document.addEventListener("DOMContentLoaded", () => {
+//   const root = document.getElementById("root");
+//   ReactDOM.render(appRouter, root);
+// });
